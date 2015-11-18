bf8d9bedc9b5a07778fb5d789bbc5200dc8f003c
Tim Uy <tim@loqu8.com>
2015-11-18 11:10:49 -0800

    swap XamarinIOS, XamarinAndroid and UWP to nunit test runner

--- tests/SQLite.Net.Tests.UWP/App.xaml.cs
+++ tests/SQLite.Net.Tests.UWP/App.xaml.cs
@@ -28,6 +28,9 @@ namespace SQLite.Net.Tests.UWP
         /// </summary>
         public App()
         {
+            Microsoft.ApplicationInsights.WindowsAppInitializer.InitializeAsync(
+                Microsoft.ApplicationInsights.WindowsCollectors.Metadata |
+                Microsoft.ApplicationInsights.WindowsCollectors.Session);
             this.InitializeComponent();
             this.Suspending += OnSuspending;
         }
@@ -58,6 +61,8 @@ namespace SQLite.Net.Tests.UWP
 
                 rootFrame.NavigationFailed += OnNavigationFailed;
 
+                Xamarin.Forms.Forms.Init(e);
+
                 if (e.PreviousExecutionState == ApplicationExecutionState.Terminated)
                 {
                     //TODO: Load state from previously suspended application
@@ -66,13 +71,16 @@ namespace SQLite.Net.Tests.UWP
                 // Place the frame in the current Window
                 Window.Current.Content = rootFrame;
             }
-            
-            Microsoft.VisualStudio.TestPlatform.TestExecutor.UnitTestClient.CreateDefaultUI();
 
+            if (rootFrame.Content == null)
+            {
+                // When the navigation stack isn't restored navigate to the first page,
+                // configuring the new page by passing required information as a navigation
+                // parameter
+                rootFrame.Navigate(typeof(MainPage), e.Arguments);
+            }
             // Ensure the current window is active
             Window.Current.Activate();
-
-            Microsoft.VisualStudio.TestPlatform.TestExecutor.UnitTestClient.Run(e.Arguments);
         }
 
         /// <summary>
